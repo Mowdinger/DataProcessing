1b713aba6f61bc4a11bf20df842c8823ed7282dd
Xiaopei <sun_xiaopei@outlook.com>
2018-03-28 21:39:00 +0800

    绘制二维色彩图

--- DataProcessing.py
+++ DataProcessing.py
@@ -3,7 +3,7 @@ import numpy as np
 import scipy as sp
 import matplotlib
 import matplotlib.pyplot as plt
-import pylab
+import pylab as plb
 
 
 def ReadData(name,path):
@@ -19,13 +19,35 @@ def ReadData(name,path):
     file.close()
     return data
 
-data=ReadData('180322.003.txt',r'C:\Users\孙晓培\AnacondaProjects\DataProcessing\180322.003.txt')
-#print(data[:,2])
-plt.figure(1)
-for i in np.linspace(1,data.shape[1],data.shape[1]):
-    plt.subplot(data.shape[1],1,i)
-    x=data[:,3]
-    y=data[:,int(i)-1]
-    plt.plot(x,y)
-    #plt.title('Column %d' %i)
+# data1=ReadData('180322.001.txt',r'C:\Users\孙晓培\AnacondaProjects\DataProcessing\180322.001.txt')
+# #print(data[:,2])
+# plt.figure(1)
+# for i in np.linspace(2,data1.shape[1],data1.shape[1]-1):
+#     plt.subplot(data1.shape[1],1,i-1)
+#     x=data1[:,0]
+#     y=data1[:,int(i)-1]
+#     plt.plot(x,y)
+#     #plt.title('Column %d' %i)
+# plt.show()
+
+data2=ReadData('180323.004.txt',r'C:\Users\孙晓培\AnacondaProjects\DataProcessing\180323.004.txt')
+
+for i in np.linspace(0,data2.shape[0]-1,data2.shape[0]):
+    if data2[int(i)][-1]==data2[0][-1]:
+        continue
+    else:
+        y_points=int(i)
+        x_points=int(data2.shape[0]/i)
+        print('There are %d points in Magnetic Filed \'B\' and %d points in \'I_bias\'.' %(y_points,x_points))
+        break
+
+plt.figure(2)
+
+x=np.linspace(data2[0][-1],data2[-1][-1],x_points)
+y=np.linspace(data2[0][3],data2[-1][3],y_points)
+X,Y=np.meshgrid(x,y)
+R=data2[:,1].reshape(x_points,y_points)
+
+plb.pcolor(X,Y,R.T)
+plb.colorbar()
 plt.show()
